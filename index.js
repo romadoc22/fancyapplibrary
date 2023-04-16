b5ced9b58569417c90b27613c998e630e6bbb5ed
romadoc22 <113574669+romadoc22@users.noreply.github.com>
2023-04-16 18:12:46 +0300

    add fix

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
-import { Carousel } from "@fancyapps/ui/dist/carousel/carousel.esm.js";
-import "@fancyapps/ui/dist/carousel/carousel.css";
-
-import "./style.css";
-
-new Carousel(
-  document.getElementById("cardSlider"),
-  {
-    Navigation: {
-      prevTpl:
-        '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M11 5l-7 7 7 7"/><path d="M4 12h16"/></svg>',
-      nextTpl:
-        '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M4 12h16"/><path d="M13 5l7 7-7 7"/></svg>',
-    },
-
-    infinite: true,
-    center: false,
-    slidesPerPage: "auto",
-    transition: false,
-  }
-);
+import { Fancybox } from "@fancyapps/ui";
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
+Fancybox.bind('[data-fancybox="gallery"]', {
+  //
+});
